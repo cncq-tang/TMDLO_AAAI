e25992f146770804ebffc6b15f4a17fa8d37d4c0
cncq.tang <18408240627@163.com>
2024-03-04 14:35:50 +0800

    文档函数注释修改

--- model.py
+++ model.py
@@ -2,14 +2,24 @@ import torch
 import torch.nn as nn
 
 
+# 模型结构
+class TMDLO(nn.Module):
+    def __init__(self, classes, views, classifier_dims):
+        """
+        :param classes:
+        :param views:
+        :param classifier_dims:
+        """
+        super().__init__()
+
+
 # 神经网络结构
 class Classifier(nn.Module):
-    """
-    classifier_dims:神经网络各层的维度，即数据集各个视角维度:[[240], [76], [216], [47], [64], [6]]
-    classes:输出的类别数量
-    """
-
     def __init__(self, classifier_dims, classes):
+        """
+        :param classifier_dims: 神经网络各层的维度，即数据集各个视角维度:[[240], [76], [216], [47], [64], [6]]
+        :param classes: 输出的类别数量
+        """
         super(Classifier, self).__init__()
         self.num_layers = len(classifier_dims)
         self.fc = nn.ModuleList()  # 创建了一个空的 nn.ModuleList() 对象 self.fc，用于存储神经网络的所有线性层
@@ -19,12 +29,11 @@ class Classifier(nn.Module):
         self.fc.append(nn.Linear(classifier_dims[self.num_layers - 1], classes))  # 最后一层分类输出层
         self.fc.append(nn.Softplus())  # 激活函数层，Relu的替代方案
 
-    """
-    x:神经网络的输入数据
-    return:神经网络的最终输出，即经过所有线性层和激活函数后的结果
-    """
-
     def forward(self, x):
+        """
+        :param x: 神经网络的输入数据
+        :return: 神经网络的最终输出，即经过所有线性层和激活函数后的结果
+        """
         h = self.fc[0](x)  # 把输入X传递给第一个线性层
         for i in range(1, len(self.fc)):
             h = self.fc[i](h)
